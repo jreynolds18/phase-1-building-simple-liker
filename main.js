9378566b040c4bbb48413961383988c5ea9f45a6
Jesse Reynolds <jessereynolds18@gmail.com>
2023-10-03 20:46:01 -0500

    build a simple liker's functionality to like something and reflect that with a full heart symbol

--- main.js
+++ main.js
@@ -3,8 +3,33 @@ const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
+// Select the heart glyphs
+const grabLikeHeart = document.querySelectorAll(".like-glyph");
 
-
+// For each heart glyph, add a click event listener that calls the server mimic
+grabLikeHeart.forEach(heart => {
+  heart.addEventListener("click", () => {
+      mimicServerCall()
+        // A then method on the event listener to first check if activated and remove on click, then if not activated, activate and fill.
+        .then(() => {
+          if (heart.classList.contains("activated-heart")) {
+            heart.classList.remove("activated-heart");
+            heart.textContent = EMPTY_HEART;
+          } else {
+          heart.classList.add("activated-heart");
+          heart.textContent = FULL_HEART;
+          }
+        })
+        // A catch method to get the error to show as requested by removing or adding the "hidden" class and set a timeout of 3 seconds to hide the error again.
+        .catch(() => {
+          const grabErrorModal = document.querySelector("#modal");
+          grabErrorModal.classList.remove("hidden");
+          setTimeout(() => {
+            grabErrorModal.classList.add("hidden");
+          }, 3000);
+        });
+  });
+});
 
 
 //------------------------------------------------------------------------------
